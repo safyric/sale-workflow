6aa8179457f33c42c01ff1d1c08e7ceb5ab4ff7d
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-06 15:18:58 +0800

    Update sale_order_line.py

--- sale_order_line_description/models/sale_order_line.py
+++ sale_order_line_description/models/sale_order_line.py
@@ -8,19 +8,32 @@ class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
     @api.multi
-    @api.onchange('product_id')
-    def product_id_change(self):
-        res = super(SaleOrderLine, self).product_id_change()
-        if not self.product_id:  # pragma: no cover
-            return res
-        if (self.user_has_groups(
-                'sale_order_line_description.'
-                'group_use_product_description_per_so_line') and
-                self.product_id.description_sale):
-            product = self.product_id
-            if self.order_id.partner_id:
-                product = product.with_context(
-                    lang=self.order_id.partner_id.lang,
-                )
-            self.name = product.description_sale
+    def _get_sale_order_line_multiline_description_sale(self, product):
+        res = super(SaleOrderLine, self)._get_sale_order_line_multiline_description_sale(product)
+        return product.get_product_multiline_description_sale() + self._get_sale_order_line_multiline_description_variants()
+
         return res
+    
+    def _get_sale_order_line_multiline_description_variants(self):
+        res1 = super(SaleOrderLine, self)._get_sale_order_line_multiline_description_variants()
+        if not self.product_custom_attribute_value_ids and not self.product_no_variant_attribute_value_ids:
+            return ""
+        
+        name = "\n"
+        
+        product_attribute_with_is_custom = self.product_custom_attribute_value_ids.mapped('attribute_value_id.attribute_id')
+        
+        # display the no_variant attributes, except those that are also
+        # displayed by a custom (avoid duplicate)
+        for no_variant_attribute_value in self.product_no_variant_attribute_value_ids.filtered(
+            lambda ptav: ptav.attribute_id not in product_attribute_with_is_custom
+        ):
+            name += "\n" + no_variant_attribute_value.attribute_id.name + ': ' + no_variant_attribute_value.name
+            
+        # display the is_custom values
+        for pacv in self.product_custom_attribute_value_ids:
+            name += "\n" + pacv.attribute_value_id.attribute_id.name + \
+            ': ' + (pacv.custom_value or '').strip()
+        return name        
+        
+        return res1
