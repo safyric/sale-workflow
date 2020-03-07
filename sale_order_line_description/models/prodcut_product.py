c2f5df85dc66564fac509d9f27763921eecc0364
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-07 19:59:20 +0800

    Delete prodcut_product.py

--- sale_order_line_description/models/prodcut_product.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-
-
-class ProductAttributeValue(models.Model):
-    _inherit = "product.attribute.value"
-
-    sale_order_line_id = fields.Many2one('sale.order.line', string='Sale order line')
