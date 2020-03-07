a38d739fee31dd416e7a24c4011772445ae2c345
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-07 19:44:04 +0800

    Update sale_order_line.py

--- sale_order_line_description/models/sale_order_line.py
+++ sale_order_line_description/models/sale_order_line.py
@@ -22,7 +22,7 @@ class SaleOrderLine(models.Model):
         res1 = super(SaleOrderLine, self)._get_sale_order_line_multiline_description_variants()
 
         if not self.product_custom_attribute_value_ids and not self.product_no_variant_attribute_value_ids:
-            return ""
+            return product_attribute_value_ids
 
         name = ""
 
