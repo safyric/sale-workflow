11e687ce33ec4da1bce15746ae05a092f87ff1c4
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-07 11:32:12 +0800

    Update sale_order_line.py

--- sale_order_line_description/models/sale_order_line.py
+++ sale_order_line_description/models/sale_order_line.py
@@ -18,38 +18,38 @@ class SaleOrderLine(models.Model):
         """
         return product.display_name + self._get_sale_order_line_multiline_description_variants()
     
-    return res
+        return res
     
     def _get_sale_order_line_multiline_description_variants(self):
         res1 = super(SaleOrderLine, self)._get_sale_order_line_multiline_description_variants()
-            """When using no_variant attributes or is_custom values, the product
-            itself is not sufficient to create the description: we need to add
-            information about those special attributes and values.
-            See note about `product_no_variant_attribute_value_ids` above the field
-            definition: this method is not reliable to recompute the description at
-            a later time, it should only be used initially.
-            :return: the description related to special variant attributes/values
-            :rtype: string
-            """
-            if not self.product_custom_attribute_value_ids and not self.product_no_variant_attribute_value_ids:
-                return ""
+        """When using no_variant attributes or is_custom values, the product
+        itself is not sufficient to create the description: we need to add
+        information about those special attributes and values.
+        See note about `product_no_variant_attribute_value_ids` above the field
+        definition: this method is not reliable to recompute the description at
+        a later time, it should only be used initially.
+        :return: the description related to special variant attributes/values
+        :rtype: string
+        """
+        if not self.product_custom_attribute_value_ids and not self.product_no_variant_attribute_value_ids:
+            return ""
 
-            name = "\n"
+        name = "\n"
 
-            product_attribute_with_is_custom = self.product_custom_attribute_value_ids.mapped('attribute_value_id.attribute_id')
+        product_attribute_with_is_custom = self.product_custom_attribute_value_ids.mapped('attribute_value_id.attribute_id')
 
-            # display the no_variant attributes, except those that are also
-            # displayed by a custom (avoid duplicate)
-            for no_variant_attribute_value in self.product_no_variant_attribute_value_ids.filtered(
-                lambda ptav: ptav.attribute_id not in product_attribute_with_is_custom
-            ):
-                name += "\n" + no_variant_attribute_value.attribute_id.name + ': ' + no_variant_attribute_value.name
+        # display the no_variant attributes, except those that are also
+        # displayed by a custom (avoid duplicate)
+        for no_variant_attribute_value in self.product_no_variant_attribute_value_ids.filtered(
+            lambda ptav: ptav.attribute_id not in product_attribute_with_is_custom
+        ):
+            name += "\n" + no_variant_attribute_value.attribute_id.name + ': ' + no_variant_attribute_value.name
 
-            # display the is_custom values
-            for pacv in self.product_custom_attribute_value_ids:
-                name += "\n" + pacv.attribute_value_id.attribute_id.name + \
-                    ': ' + (pacv.custom_value or '').strip()
-
-            return name
+        # display the is_custom values
+        for pacv in self.product_custom_attribute_value_ids:
+            name += "\n" + pacv.attribute_value_id.attribute_id.name + \
+                ': ' + (pacv.custom_value or '').strip()
+            
+        return name
         
         return res1
