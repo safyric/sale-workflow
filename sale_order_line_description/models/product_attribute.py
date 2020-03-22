1e2e29c82955125451a1a385b77c6d0a9601d7d1
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-22 17:15:48 +0800

    Create product_attribute.py

---
+++ sale_order_line_description/models/product_attribute.py
@@ -0,0 +1,6 @@
+# Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+class ProductAttributeValue(models.Model):
