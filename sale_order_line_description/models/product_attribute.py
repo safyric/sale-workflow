240f9a7529a283df5e96e0a66a7f8299ce708fb9
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-22 18:14:59 +0800

    Update product_attribute.py

--- sale_order_line_description/models/product_attribute.py
+++ sale_order_line_description/models/product_attribute.py
@@ -7,9 +7,3 @@ class ProductAttributeValue(models.Model):
     _inherit = "product.attribute.value"
 
     short_name = fields.Char(string='Value', required=True, translate=True)
-
-    @api.multi
-    def _variant_name(self, variable_attributes):
-        res = super(ProductAttributeValue,self)._variant_name(variable_attributes)
-        return ", ".join([v.short_name for v in self if v.attribute_id in variable_attributes])
-        return res
