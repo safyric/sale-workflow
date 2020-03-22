586d59b1c8dd5b7fc2576d15838ab947dc9cf1fa
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-22 19:41:24 +0800

    Update product_attribute.py

--- sale_order_line_description/models/product_attribute.py
+++ sale_order_line_description/models/product_attribute.py
@@ -3,6 +3,12 @@
 
 from odoo import api, fields, models
 
+class ProductAttribute(models.Model):
+    _inherit = "product.attribute"
+
+    description = fields.Char('Description', translate=True)
+    
+
 class ProductAttributeValue(models.Model):
     _inherit = "product.attribute.value"
 
