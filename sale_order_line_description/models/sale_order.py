81342b3afbf2b36e437917f074548f06b4c87324
jchanme <39323414+jchanme@users.noreply.github.com>
2020-03-24 22:34:01 +0800

    Update sale_order.py

--- sale_order_line_description/models/sale_order.py
+++ sale_order_line_description/models/sale_order.py
@@ -1,4 +1,3 @@
-
 # Copyright 2013-15 Agile Business Group sagl (<http://www.agilebg.com>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
@@ -13,6 +12,7 @@ class SaleOrder(models.Model):
 class SaleOrderLine(models.Model):
     _inherit = "sale.order.line"
 
+
     @api.multi
     def get_sale_order_line_multiline_description_sale(self, product):
         res = super(SaleOrderLine,self).get_sale_order_line_multiline_description_sale(product)
@@ -23,51 +23,52 @@ class SaleOrderLine(models.Model):
 
         return res
 
-    product_attribute_value_ids = fields.Many2many('product.attribute.value', string='Product attributes')
     def _get_sale_order_line_multiline_description_variants(self):
-        res1 = super(SaleOrderLine, self)._get_sale_order_line_multiline_description_variants()
+        res1 = super(SaleOrderLine,self)._get_sale_order_line_multiline_description_variants()
         name = ""
 
         product_attribute_with_is_custom = self.product_custom_attribute_value_ids.mapped('attribute_value_id.attribute_id')
-        for attribute_value_with_variant in self.product_attribute_value_ids.filtered(
-            lambda ptav: ptav.attribute_id not in product_attribute_with_is_custom
-        ):
-            if self.order_id.is_long_description:
-                if no_variant_attribute_value.attribute_id.description and no_variant_attribute_value.attribute_id.is_reverse_description == False:
-                    name += no_variant_attribute_value.name + " " + no_variant_attribute_value.attribute_id.description + ", "
-                elif no_variant_attribute_value.attribute_id.description and no_variant_attribute_value.attribute_id.is_reverse_description == True:
-                    name += no_variant_attribute_value.attribute_id.description + " " + no_variant_attribute_value.name + ", "
-                else:
-                    name += no_variant_attribute_value.name + ", "
-            else:
-                name += no_variant_attribute_value.attribute_id.name + ': ' + no_variant_attribute_value.name + "\n"
-                
-        # display the no_variant attributes, except those that are also
-        # displayed by a custom (avoid duplicate)
-        for no_variant_attribute_value in self.product_no_variant_attribute_value_ids.filtered(
-            lambda ptav: ptav.attribute_id not in product_attribute_with_is_custom
-        ):
-            if self.order_id.is_long_description:
-                if no_variant_attribute_value.attribute_id.description and no_variant_attribute_value.attribute_id.is_reverse_description == False:
-                    name += no_variant_attribute_value.name + " " + no_variant_attribute_value.attribute_id.description + ", "
-                elif no_variant_attribute_value.attribute_id.description and no_variant_attribute_value.attribute_id.is_reverse_description == True:
-                    name += no_variant_attribute_value.attribute_id.description + " " + no_variant_attribute_value.name + ", "
-                else:
-                    name += no_variant_attribute_value.name + ", "
-            else:
-                name += no_variant_attribute_value.attribute_id.name + ': ' + no_variant_attribute_value.name + "\n"
 
-        # display the is_custom values
+        # attribute_value_with_variants
+        for pav in self.product_id.attribute_value_ids.filtered(
+            lambda pav: pav.attribute_id not in product_attribute_with_is_custom
+        ):
+            if self.order_id.is_long_description:
+                if pav.attribute_id.description and pav.attribute_id.is_reverse_description == False:
+                    name += pav.name + " " + pav.attribute_id.description + ", "
+                elif pav.attribute_id.description and pav.attribute_id.is_reverse_description == True:
+                    name += pav.attribute_id.description + " " + pav.name + ", "
+                else:
+                    name += pav.name + ", "
+            else:
+                name += pav.attribute_id.name + ': ' + pav.name + "\n"
+
+        # no_variant_attribute_value
+        for pvav in self.product_no_variant_attribute_value_ids.filtered(
+            lambda pvav: pvav.attribute_id not in product_attribute_with_is_custom
+        ):
+            if self.order_id.is_long_description:
+                if pvav.attribute_id.description and pvav.attribute_id.is_reverse_description == False:
+                    name += pvav.name + " " + pvav.attribute_id.description + ", "
+                elif pvav.attribute_id.description and pvav.attribute_id.is_reverse_description == True:
+                    name += pvav.attribute_id.description + " " + pvav.name + ", "
+                else:
+                    name += pvav.name + ", "
+            else:
+                name += pvav.attribute_id.name + ': ' + pvav.name + "\n"
+
+        # product_custom_attribute_value
         for pacv in self.product_custom_attribute_value_ids:
             if self.order_id.is_long_description:
-                if pacv.attribute_value_id.description and pacv.attribute_value_id.is_reverse_description == False:
-                    name += (pacv.custom_value or '').strip() + " " + pacv.attribute_value_id.attribute_id.name + ", "
-                elif pacv.attribute_value_id.description and pacv.attribute_value_id.is_reverse_description == True:
-                    name += pacv.attribute_value_id.attribute_id.name + " " + (pacv.custom_value or '').strip() + ", "
+                if pacv.attribute_value_id.attribute_id.description and pacv.attribute_value_id.attribute_id.is_reverse_description == False:
+                    name += (pacv.custom_value or '').strip() + " " + pacv.attribute_value_id.attribute_id.description + ", "
+                elif pacv.attribute_value_id.attribute_id.description and pacv.attribute_value_id.attribute_id.is_reverse_description == True:
+                    name += pacv.attribute_value_id.attribute_id.description + " " + (pacv.custom_value or '').strip() + ", "
                 else:
                     name += (pacv.custom_value or '').strip() + ", "
             else:
-                name += pacv.attribute_value_id.attribute_id.name + ': ' + (pacv.custom_value or '').strip() + "\n"
+                name += pacv.attribute_value_id.attribute_id.name + \
+                ': ' + (pacv.custom_value or '').strip() + "\n"
 
         return name
 
